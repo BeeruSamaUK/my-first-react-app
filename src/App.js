9f1a27424f39274e25e5f59efc92dd75a7c198d3
BeerusSama <robisguess@gmail.com>
2020-02-01 21:34:49 +0000

    updated my cart

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import ChekoutPage from './pages/checkout/checkout.component';
+
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './components/firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
@@ -45,6 +47,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={ChekoutPage} />
           <Route
             exact
             path='/signin'
