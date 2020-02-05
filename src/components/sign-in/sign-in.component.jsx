8517e62bdeb564e99a24b8946fdf9169c48b7e91
BeerusSama <robisguess@gmail.com>
2020-02-05 19:30:13 +0000

    break homepage to smaller components

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -17,18 +17,18 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit =  async event => {
+  handleSubmit = async event => {
     event.preventDefault();
 
     const { email, password } = this.state;
 
-try {
-  await auth.signInWithEmailAndPassword(email, password);
-  this.setState({ email: '', password: '' });
- } catch (error) {
-   console.log(error);
- }
-};
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   handleChange = event => {
     const { value, name } = event.target;
